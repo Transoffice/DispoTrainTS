29a8be7beddf6e1b41f064e7bb65486f335ce1d5
Transoffice <san@mytransoffice.de>
2013-09-02 08:51:16 +0200

    02.09.2013ab

--- Classes/TypeScript/ContainerBase.ts
+++ Classes/TypeScript/ContainerBase.ts
@@ -37,9 +37,6 @@ class Container {
         pcanvas.fillText (this.cCnumber, 100, 100);
       
 
-
-
-
     }
 
 }
@@ -48,32 +45,29 @@ window.onload = () => {
 
     var ErsterContainer = <HTMLCanvasElement> document.getElementById('ErsterContainer');
     var ZweiterContainer = <HTMLCanvasElement> document.getElementById('ZweiterContainer');
-    var Canvas1 = <HTMLCanvasElement> document.getElementById('Canvas1');
-    var Canvas2 = <HTMLCanvasElement> document.getElementById('Canvas2');
     var Canvas3 = <HTMLCanvasElement> document.getElementById('Canvas3');
-    var Canvas4= <HTMLCanvasElement> document.getElementById('Canvas4');
+    var Canvas4 = <HTMLCanvasElement> document.getElementById('Canvas4');
+    var Canvas5 = <HTMLCanvasElement> document.getElementById('Canvas5');
+    var Canvas6= <HTMLCanvasElement> document.getElementById('Canvas6');
+      
 
-    
-  
-    
-
-    var container = new Container('', 'RTSB123456-7','20box', 'black', 605, 259, 'white');
+    var container = new Container('', 'RTSB123456-7','20box', 'black', 605.8, 259.1, 'white');
     container.Paint(ErsterContainer.getContext('2d'));
 
-    var container = new Container('', 'RTSB123456-8', '20box', 'blue', 605, 259, 'white');
+    var container = new Container('', 'RTSB123456-8', '20box', 'blue', 605.8, 259.1, 'white');
     container.Paint(ZweiterContainer.getContext('2d'));
 
-    var container = new Container('', 'RTSB123456-9', '20box', 'blue', 605, 259, 'white');
-    container.Paint(Canvas1.getContext('2d'));
-
-    var container = new Container('', 'RTSB123456-1', '20box', 'blue', 605, 259, 'white');
-    container.Paint(Canvas2.getContext('2d'));
-
-    var container = new Container('', 'RTSB123456-2', '20box', 'blue', 605, 259, 'white');
+    var container = new Container('', 'RTSB123456-9', '40box', 'red', 1219.2, 259.1, 'white');
     container.Paint(Canvas3.getContext('2d'));
 
-    var container = new Container('', 'RTSB123456-3', '20box', 'blue', 605, 259, 'white');
+    var container = new Container('', 'RTSB123456-1', '20box', 'blue', 605.8, 259.1, 'white');
     container.Paint(Canvas4.getContext('2d'));
+
+    var container = new Container('', 'RTSB123456-2', '20box', 'blue', 605.8, 259.1, 'white');
+    container.Paint(Canvas5.getContext('2d'));
+
+    var container = new Container('', 'RTSB123456-3', '20box', 'blue', 605.8, 259.1, 'white');
+    container.Paint(Canvas6.getContext('2d'));
 
   
 
